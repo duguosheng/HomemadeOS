5752c5b8be0369cf57144dce9ac6d7aac0cb2916
duguosheng <duguosheng99@foxmail.com>
2022-05-04 19:01:47 +0800

    04: harib01d-数组和指针

--- 04_day/bootpack.c
+++ 04_day/bootpack.c
@@ -6,7 +6,7 @@ void HariMain(void)
     int i;
     char *p = (char *)0xa0000;
     for (i = 0x0; i < 0xffff; ++i) {
-        *(p + i) = i & 0x0f;
+        p[i] = i & 0x0f;
     }
     for (;;) {
         io_hlt();
