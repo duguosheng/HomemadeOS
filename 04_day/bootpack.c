d36d1e64cdcc74bddfaffc4763bebefaf4f6f0ae
duguosheng <duguosheng99@foxmail.com>
2022-05-04 19:01:18 +0800

    04: harib01d-简化指针使用

--- 04_day/bootpack.c
+++ 04_day/bootpack.c
@@ -4,10 +4,9 @@ void io_hlt(void);
 void HariMain(void)
 {
     int i;
-    char *p;
-    for (i = 0xa0000; i < 0xaffff; ++i) {
-        p = (char *)i;
-        *p = i & 0x0f;  // 替换write_mem8(i, i & 0x0f)
+    char *p = (char *)0xa0000;
+    for (i = 0x0; i < 0xffff; ++i) {
+        *(p + i) = i & 0x0f;
     }
     for (;;) {
         io_hlt();
