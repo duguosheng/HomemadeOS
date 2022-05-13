c57cd5f2b21eb47552c57ae69e34930f552ad0a2
duguosheng <duguosheng99@foxmail.com>
2022-05-13 09:56:47 +0800

    09: harib07c-加快图层处理速度1

--- 10_day/sheet.c
+++ 10_day/sheet.c
@@ -69,6 +69,43 @@ void sheet_setbuf(struct SHEET *sht, unsigned char *buf, int xsize, int ysize, i
     sht->col_inv = col_inv;
 }
 
+
+/**
+ * @brief 刷新描绘指定坐标范围内的所有图层
+ *
+ * @param ctl 图层管理结构体
+ * @param vx0 左上角的x坐标
+ * @param vy0 左上角的y坐标
+ * @param vx1 右下角的x坐标
+ * @param vy1 右下角的y坐标
+ */
+void sheet_refreshsub(struct SHTCTL *ctl, int vx0, int vy0, int vx1, int vy1)
+{
+    int h, bx, by, vx, vy;
+    unsigned char *buf, c, *vram = ctl->vram;
+    struct SHEET *sht;
+    // 从下往上绘制所有图层，上面的就会覆盖掉下面的，达到层级效果
+    for (h = 0; h <= ctl->top; h++) {
+        sht = ctl->sheets[h];
+        buf = sht->buf;
+        for (by = 0; by < sht->bysize; by++) {
+            vy = sht->vy0 + by;
+            for (bx = 0; bx < sht->bxsize; bx++) {
+                vx = sht->vx0 + bx;
+                if (vx0 <= vx && vx < vx1 && vy0 <= vy && vy < vy1) {
+                    c = buf[by * sht->bxsize + bx];
+                    // 只有当前的颜色不是透明色号时才进行渲染
+                    // 例如对于鼠标来说，箭头以外的色号是99，而col_inv也是99
+                    // 那么就不对其进行渲染，从而不覆盖下面图层的颜色
+                    if (c != sht->col_inv) {
+                        vram[vy * ctl->xsize + vx] = c;
+                    }
+                }
+            }
+        }
+    }
+}
+
 /**
  * @brief 设置图层高度
  *
@@ -111,7 +148,7 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
             }
             ctl->top--;
         }
-        sheet_refresh(ctl);
+        sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
     } else if (old < height) {
         // 比之前高
         if (old >= 0) {
@@ -131,37 +168,24 @@ void sheet_updown(struct SHTCTL *ctl, struct SHEET *sht, int height)
             ctl->sheets[height] = sht;
             ctl->top++; // 由于显示状态的图层增加了一个，所以最上面的图层高度增加
         }
-        sheet_refresh(ctl);
+        sheet_refreshsub(ctl, sht->vx0, sht->vy0, sht->vx0 + sht->bxsize, sht->vy0 + sht->bysize);
     }
 }
 
 /**
- * @brief 刷新描绘所有图层
+ * @brief 刷新描绘相对位置范围内的所有图层
  *
  * @param ctl 图层管理结构体
+ * @param sht 指定的图层（用于相对位置参考）
+ * @param bx0 图层内左上角的x坐标
+ * @param by0 图层内左上角的y坐标
+ * @param bx1 图层内右下角的x坐标
+ * @param by1 图层内右下角的y坐标
  */
-void sheet_refresh(struct SHTCTL *ctl)
+void sheet_refresh(struct SHTCTL *ctl, struct SHEET *sht, int bx0, int by0, int bx1, int by1)
 {
-    int h, bx, by, vx, vy;
-    unsigned char *buf, c, *vram = ctl->vram;
-    struct SHEET *sht;
-    // 从下往上绘制所有图层，上面的就会覆盖掉下面的，达到层级效果
-    for (h = 0; h <= ctl->top; h++) {
-        sht = ctl->sheets[h];
-        buf = sht->buf;
-        for (by = 0; by < sht->bysize; by++) {
-            vy = sht->vy0 + by;
-            for (bx = 0; bx < sht->bxsize; bx++) {
-                vx = sht->vx0 + bx;
-                c = buf[by * sht->bxsize + bx];
-                // 只有当前的颜色不是透明色号时才进行渲染
-                // 例如对于鼠标来说，箭头以外的色号是99，而col_inv也是99
-                // 那么就不对其进行渲染，从而不覆盖下面图层的颜色
-                if (c != sht->col_inv) {
-                    vram[vy * ctl->xsize + vx] = c;
-                }
-            }
-        }
+    if (sht->height >= 0) { /* 如果正在显示，则按新图层的信息刷新画面 */
+        sheet_refreshsub(ctl, sht->vx0 + bx0, sht->vy0 + by0, sht->vx0 + bx1, sht->vy0 + by1);
     }
 }
 
@@ -175,10 +199,16 @@ void sheet_refresh(struct SHTCTL *ctl)
  */
 void sheet_slide(struct SHTCTL *ctl, struct SHEET *sht, int vx0, int vy0)
 {
+    int old_vx0 = sht->vx0;
+    int old_vy0 = sht->vy0;
     sht->vx0 = vx0;
     sht->vy0 = vy0;
-    if (sht->height >= 0) { /* 如果正在显示*/ 
-        sheet_refresh(ctl); /* 按新图层的信息刷新画面 */ 
+    if (sht->height >= 0) { /* 如果正在显示，则按新图层的信息刷新画面 */
+        // 图层位置改变后需要重绘两个区域
+        // 1.图层原来所在区域
+        sheet_refreshsub(ctl, old_vx0, old_vy0, old_vx0 + sht->bxsize, old_vy0 + sht->bysize);
+        // 2.图层当前所在区域
+        sheet_refreshsub(ctl, vx0, vy0, vx0 + sht->bxsize, vy0 + sht->bysize);
     }
     return;
 }
